edba56a0acfc48bd7bbd765e9132c44521bf4c99
zhenpanda <zhentzhou@gmail.com>
2016-02-21 00:59:14 -0500

    keeplearning

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Book = require('./model/book.model')
 
 //Mongoose Connect
-mongoose.connect('mongodb://localhost/manabasehelper');
-var db = mongoose.connection;
+var db = 'mongodb://localhost/manabasehelper';
+mongoose.connect(db);
 
 app.use(express.static(__dirname+'/client'));
 app.use(bodyParser.json());
@@ -14,5 +15,17 @@ app.get('/', function(req, res) {
 	res.send("harro world");
 });
 
-app.listen(1374);
-console.log("listening on port 1374 captain")
+app.get('/check', function(req, res) {
+	Book.find({}).exec(function(err, result) {
+		if(err) {
+			res.send('error here.')
+		}else{
+			res.json(result);
+		}
+	})
+});
+
+var port = 3000;
+app.listen(port, function() {
+	console.log("listenin on port:" + port);
+})
--- model/book.model.js
+++ model/book.model.js
@@ -5,18 +5,33 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-// var BookSchema = new Schema({
-// 	title: String,
-// 	keywords: Array,
-// 	published: Boolean
-// });
-
 var BookSchema = new Schema({
 	title: String,
-	title: {
-		type: String,
-		require: true
-	}
-})
+	author: String,
+	category: String
+});
 
+// var BookSchema = new Schema({
+// 	title: String,
+// 	published: {
+// 		type: Date,
+// 		default: Date.now
+// 	},
+// 	keywords: Array,
+// 	published: Boolean,
+// 	author: {
+// 		type: Schema.ObjectId,
+// 		ref:'User'
+// 	},
+// 	detail: {
+// 		modelNumber: Number,
+// 		hardcover: Boolean,
+// 		reviews: Number,
+// 		rank: Number
+// 	}
+// })
+
+
+
+//export pass in schema
 module.exports = mongoose.model('Book', BookSchema);
